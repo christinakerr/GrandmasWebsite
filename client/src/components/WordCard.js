a5a865c47bf28cecbb2ba779a991af6b8a329b3e
Christi Kerr <christinakerr394@gmail.com>
2020-12-29 10:55:21 -0600

    Built basic word component

--- client/src/components/WordCard.js
+++ client/src/components/WordCard.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Card } from "reactstrap";
 
 function WordCard(){
     return (
         <>
+        <Card>
+            <h1>Words go here</h1>
+        </Card>
         </>
     )
 }
