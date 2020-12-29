7b94ec463784c9b12a4f176b84b9df601f4d4a69
Christi Kerr <christinakerr394@gmail.com>
2020-12-29 11:42:28 -0600

    onclick function to play audio when word is clicked

--- client/src/components/WordCard.js
+++ client/src/components/WordCard.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { Card } from "reactstrap";
+import audio from "../audio/chicken.mp3"
 
 function WordCard(){
+
+    let chicken = new Audio(audio)
+    
+    const play = () => {
+        chicken.play()
+        .then(console.log("Working"))
+        .catch(err => console.log(err))
+    }
+
+    // window.addEventListener("click", e=> {
+    //     let audio = document.getElementById("chicken");
+    //     if (!audio){
+    //         return;
+    //     }
+    //     console.log(audio);
+    //     audio.play();
+    // })
     return (
         <>
-        <Card>
-            <h1>Words go here</h1>
+        <Card >
+            <button onClick={play}>
+                <h1>Chicken</h1>
+                {/* <audio src={chicken} id="chicken"/> */}
+            </button>
+            
         </Card>
         </>
     )
