759e3c349c40a6aae55362c1bebb8f1c4adb0547
Christi Kerr <christinakerr394@gmail.com>
2021-03-03 11:07:33 -0600

    Refactored the word button styles into a styled component

--- src/components/WordCard.js
+++ src/components/WordCard.js
@@ -1,7 +1,21 @@
 import React from "react";
 import { Card } from "reactstrap";
+import styled from 'styled-components';
 
 function WordCard(props){
+    
+
+const WordButton = styled.button`
+    background-color: #9CC3D5FF;
+    border: none;
+    color: black;
+    margin: 5px;
+    padding: 10px;
+    box-shadow: 0px 8px 15px 0px rgba(0,0,0,0.37);
+    :focus, :active {
+        background-color: #D3DBDF;
+    }
+`
 
     // Play audio when word is clicked
     let wordAudio = new Audio(props.audio)
@@ -13,9 +27,9 @@ function WordCard(props){
     return (
         <>
         <Card className="wordCard">
-            <button className="wordButton" onClick={play}>
+            <WordButton onClick={play}>
                 <h2>{props.word}</h2>
-            </button>
+            </WordButton>
             
         </Card>
         </>
